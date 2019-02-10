750ac092727b35b68d8a0c712d112054667e28ca
Shahin Firouzbakht <shahin.firouzbakht@gmail.com>
2019-02-09 18:41:19 -0800

    ios,websocket

---
+++ EmergencyCoordinator-iOS/Unity/Assets/Scripts/GameManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class GameManager : Singleton<GameManager> {
+
+	public void Start() {
+
+	}
+
+	public void Send() {
+		GetComponent<Socket>().Send("its fuckin treway");
+	}
+
+	public void Update() {
+		//Debug.Log(MicInput.MicLoudness);
+	}
+
+
+}
